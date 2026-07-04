be60ead5b0cca31c438ea5ce3a5ea0e7792be351
eyob <eyob.tadesse@astu.edu.et>
2026-07-04 22:21:11 +0300

    employee controller added and also emloyee service

---
+++ src/main/java/com/HumanResourceManagement/application/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.HumanResourceManagement.application.controller;
+
+import com.HumanResourceManagement.application.model.Employee;
+import com.HumanResourceManagement.application.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/employees")
+public class EmployeeController {
+    private final EmployeeService employeeService;
+    @GetMapping
+    public List<Employee> getAllemployees(){
+        return employeeService.fetchAllEmployees();
+    }
+    @PostMapping
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee){
+        Employee addedEmployee=employeeService.createEmployee(employee);
+        return ResponseEntity.ok(addedEmployee);
+    }
+}
---
+++ src/main/java/com/HumanResourceManagement/application/service/EmployeeService.java
@@ -0,0 +1,15 @@
+package com.HumanResourceManagement.application.service;
+
+import com.HumanResourceManagement.application.model.Employee;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeService {
+    public List<Employee> fetchAllEmployees() {
+
+    }
+}
