3c62cd02647a718e8f7877fb8a4bd46802c5d463
eyob <eyob.tadesse@astu.edu.et>
2026-07-04 22:05:56 +0300

    employee repository added

---
+++ src/main/java/com/HumanResourceManagement/application/repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.HumanResourceManagement.application.repository;
+
+import com.HumanResourceManagement.application.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
+
+}
