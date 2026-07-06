f9c2324850288e1f37e94d4f6929ac1204feab92
eyob <eyob.tadesse@astu.edu.et>
2026-07-06 12:36:10 +0300

    making onetoone relationship between department and manager

--- src/main/java/com/HumanResourceManagement/application/model/Department.java
+++ src/main/java/com/HumanResourceManagement/application/model/Department.java
@@ -1,7 +1,9 @@
 package com.HumanResourceManagement.application.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -9,12 +11,18 @@ import java.time.LocalDateTime;
 
 @Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable=false,unique = true)
     private String name;
+    //making a department to be managed by a single manager
+    @OneToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="manager_id")
+    private Employee manager;
 
     private String Description;
 
