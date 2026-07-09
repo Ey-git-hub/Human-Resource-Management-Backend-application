68c0976dd09db5981a74e4d7544db7dc3ce72db5
eyob <eyob.tadesse@astu.edu.et>
2026-07-09 09:53:51 +0300

    added get departments by id endpoint

--- src/main/java/com/HumanResourceManagement/application/controller/DepartmentController.java
+++ src/main/java/com/HumanResourceManagement/application/controller/DepartmentController.java
@@ -5,6 +5,7 @@ import com.HumanResourceManagement.application.service.DepartmentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,10 +14,14 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/departments")
-public class DepartmentContoller {
+public class DepartmentController {
     private final DepartmentService departmentService;
   @GetMapping
     public ResponseEntity<List<DepartmentResponse>> getAllDepartments(){
       return ResponseEntity.ok(departmentService.fetchAllDepartments());
+  }
+  @GetMapping("/{id}")
+    public ResponseEntity<DepartmentResponse> getDepartmentById(@PathVariable Long id){
+
   }
 }
