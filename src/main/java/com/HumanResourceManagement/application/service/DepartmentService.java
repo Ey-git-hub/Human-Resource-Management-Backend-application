6d7a19f1642165ad7c0a522bb5c57f33a309ebef
eyob <eyob.tadesse@astu.edu.et>
2026-07-09 09:47:31 +0300

    implementation added tothe fetch method in department service

--- src/main/java/com/HumanResourceManagement/application/service/DepartmentService.java
+++ src/main/java/com/HumanResourceManagement/application/service/DepartmentService.java
@@ -1,13 +1,21 @@
 package com.HumanResourceManagement.application.service;
 
 import com.HumanResourceManagement.application.dto.DepartmentResponse;
+import com.HumanResourceManagement.application.repository.DepartmentRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class DepartmentService {
+    private final DepartmentRepository departmentRepository;
 
     public List<DepartmentResponse> fetchAllDepartments() {
+        return departmentRepository.findAll().stream()
+                .map(DepartmentResponse::fromEntity)
+                .collect(Collectors.toList());
     }
 }
