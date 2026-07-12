05f7d6ec1fb5bb22110f02a9edac90e0b6767075
eyob <eyob.tadesse@astu.edu.et>
2026-07-12 11:15:26 +0300

    added the required annotations for attendance service and also created the cooresponding methods from the controller

--- src/main/java/com/HumanResourceManagement/application/service/AttendanceService.java
+++ src/main/java/com/HumanResourceManagement/application/service/AttendanceService.java
@@ -1,5 +1,38 @@
 package com.HumanResourceManagement.application.service;
 
+import java.util.Optional;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import com.HumanResourceManagement.application.dto.AttendanceRequest;
+import com.HumanResourceManagement.application.dto.AttendanceResponse;
+
+import lombok.RequiredArgsConstructor;
+
+// import org.jspecify.annotations.Nullable;
+@Service
+@RequiredArgsConstructor
 public class AttendanceService {
+
+    public List<AttendanceResponse> getAllAttendance() {
+
+    }
+
+    public Optional<AttendanceResponse> getAttendanceById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAttendanceById'");
+    }
+
+    public AttendanceResponse addNewAttendance(AttendanceRequest request) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addNewAttendance'");
+    }
+
+    public @Nullable Object updateAttendance(Long id, AttendanceRequest request) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateAttendance'");
+    }
 
 }
