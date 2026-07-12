0f5df7acb70154a2af1a2b93230dc13a9820a7e1
eyob <eyob.tadesse@astu.edu.et>
2026-07-12 10:21:33 +0300

    updated attendance request dto

--- src/main/java/com/HumanResourceManagement/application/dto/AttendanceRequest.java
+++ src/main/java/com/HumanResourceManagement/application/dto/AttendanceRequest.java
@@ -1,4 +1,21 @@
 package com.HumanResourceManagement.application.dto;
 
+import java.time.LocalDate;
+// import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+// import org.hibernate.annotations.CreationTimestamp;
+
+import com.HumanResourceManagement.application.model.AttendanceStatus;
+// import com.HumanResourceManagement.application.model.Employee;
+
 public class AttendanceRequest {
+
+    private Long id;
+    private String employeeName;
+    private LocalDate date;
+    private LocalTime checkInTime;
+    private LocalTime checkOutTime;
+    private AttendanceStatus status;
+
 }
