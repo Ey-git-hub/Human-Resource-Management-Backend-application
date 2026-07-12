67aeb36a88b89eef62e27419f1502f176f10a244
eyob <eyob.tadesse@astu.edu.et>
2026-07-12 10:26:37 +0300

    updated attendance response dto

--- src/main/java/com/HumanResourceManagement/application/dto/AttendanceResponse.java
+++ src/main/java/com/HumanResourceManagement/application/dto/AttendanceResponse.java
@@ -1,4 +1,27 @@
 package com.HumanResourceManagement.application.dto;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.HumanResourceManagement.application.model.AttendanceStatus;
+import com.HumanResourceManagement.application.model.Employee;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.FetchType;
+
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
 public class AttendanceResponse {
+    private Long id;
+    private Employee employee;
+    private LocalDate date;
+    private LocalTime checkInTime;
+    private LocalTime checkOutTime;
+    private AttendanceStatus status;
+    private LocalDateTime createdAt;
+
 }
