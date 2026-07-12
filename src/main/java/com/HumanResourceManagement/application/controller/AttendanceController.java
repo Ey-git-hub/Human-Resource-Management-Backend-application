99c89d6f5a1da3d42d6a35312892c8a6776704b8
eyob <eyob.tadesse@astu.edu.et>
2026-07-12 11:01:27 +0300

    added post mapping in attendance controller

--- src/main/java/com/HumanResourceManagement/application/controller/AttendanceController.java
+++ src/main/java/com/HumanResourceManagement/application/controller/AttendanceController.java
@@ -3,6 +3,7 @@ package com.HumanResourceManagement.application.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.HumanResourceManagement.application.dto.AttendanceRequest;
 import com.HumanResourceManagement.application.dto.AttendanceResponse;
 // import com.HumanResourceManagement.application.model.AttendanceStatus;
 import com.HumanResourceManagement.application.service.AttendanceService;
@@ -11,10 +12,13 @@ import lombok.RequiredArgsConstructor;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequiredArgsConstructor
@@ -35,6 +39,12 @@ public class AttendanceController {
             return ResponseEntity.ok(result.get());
         }
         return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<AttendanceResponse> newAttendance(@RequestBody AttendanceRequest request) {
+        AttendanceResponse created = attendanceService.addNewAttendance(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
     }
 
 }
