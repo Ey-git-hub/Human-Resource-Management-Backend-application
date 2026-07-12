44f5f569c2c4db7310b1b8700e6c09aa4d7a1357
eyob <eyob.tadesse@astu.edu.et>
2026-07-12 10:51:33 +0300

    added get mapping in attendance controller

--- src/main/java/com/HumanResourceManagement/application/controller/AttendanceController.java
+++ src/main/java/com/HumanResourceManagement/application/controller/AttendanceController.java
@@ -3,14 +3,25 @@ package com.HumanResourceManagement.application.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.HumanResourceManagement.application.model.AttendanceStatus;
+import com.HumanResourceManagement.application.dto.AttendanceResponse;
+// import com.HumanResourceManagement.application.model.AttendanceStatus;
+import com.HumanResourceManagement.application.service.AttendanceService;
 
 import lombok.RequiredArgsConstructor;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/attendance")
 public class AttendanceController {
-    private final AttendanceStatus attendanceStatus;
+    private final AttendanceService attendanceService;
+
+    @GetMapping
+    public ResponseEntity<List<AttendanceResponse>> getMethodName() {
+        return ResponseEntity.ok(attendanceService.getAllAttendance());
+    }
 
 }
