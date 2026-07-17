8824d60275fcbf7490767f80a62bea5a6ec26c1a
eyob <eyob.tadesse@astu.edu.et>
2026-07-17 23:06:36 +0300

    added get all leave request endpoint

--- src/main/java/com/HumanResourceManagement/Leave/Controller/LeaveController.java
+++ src/main/java/com/HumanResourceManagement/Leave/Controller/LeaveController.java
@@ -5,10 +5,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.util.List;
 import com.HumanResourceManagement.Leave.DTO.LeaveRequest;
 import com.HumanResourceManagement.Leave.DTO.LeaveResponse;
+import com.HumanResourceManagement.Leave.Model.LeaveStatus;
 import com.HumanResourceManagement.Leave.Services.LeaveService;
 
 import jakarta.validation.Valid;
@@ -23,5 +25,11 @@ public class LeaveController {
     @PostMapping
     public ResponseEntity<LeaveResponse> createLeaveRequest(@Valid @RequestBody LeaveRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(leaveService.createLeaveRequest(request));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<LeaveResponse>> getAllLeaveRequests(
+            @RequestParam(required = false) LeaveStatus status) {
+        return ResponseEntity.ok(leaveService.getAllLeaveRequests(status));
     }
 }
