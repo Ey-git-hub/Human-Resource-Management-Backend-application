62c1f2ab1f5ab4d8880612639cf8aabd88f3f86a
eyob <eyob.tadesse@astu.edu.et>
2026-07-17 09:14:58 +0300

    updated the id generation type to identity

--- src/main/java/com/HumanResourceManagement/Training/TrainingSession.java
+++ src/main/java/com/HumanResourceManagement/Training/TrainingSession.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 public class TrainingSession {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
