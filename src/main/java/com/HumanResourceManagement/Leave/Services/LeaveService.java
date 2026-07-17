afaf14dec42dde17fa49d3ba2dc2767ef0c3a68e
eyob <eyob.tadesse@astu.edu.et>
2026-07-17 22:14:49 +0300

    modified the leave service by adding the neccessary imports

--- src/main/java/com/HumanResourceManagement/Leave/Services/LeaveService.java
+++ src/main/java/com/HumanResourceManagement/Leave/Services/LeaveService.java
@@ -1,5 +1,18 @@
 package com.HumanResourceManagement.Leave.Services;
 
+import org.springframework.stereotype.Service;
+
+import com.HumanResourceManagement.Employee.Repository.EmployeeRepository;
+import com.HumanResourceManagement.Leave.Mapper.LeaveMapper;
+import com.HumanResourceManagement.Leave.Repository.LeaveRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class LeaveService {
+    final private LeaveRepository leaveRepository;
+    final private EmployeeRepository employeeRepository;
+    final private LeaveMapper leaveMapper;
 
 }
