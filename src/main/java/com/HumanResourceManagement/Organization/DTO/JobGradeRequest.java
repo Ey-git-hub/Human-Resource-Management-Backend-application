8a4fbbe9ebe7d9fb367cc81b737bb752f507757d
eyob <eyob.tadesse@astu.edu.et>
2026-07-27 17:46:23 +0300

    update on jobgrade request

--- src/main/java/com/HumanResourceManagement/Organization/DTO/JobGradeRequest.java
+++ src/main/java/com/HumanResourceManagement/Organization/DTO/JobGradeRequest.java
@@ -1,21 +1,39 @@
 package com.HumanResourceManagement.Organization.DTO;
 
-import lombok.*;
+import com.HumanResourceManagement.Organization.Model.JobGrade;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
 import java.math.BigDecimal;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-
 public class JobGradeRequest {
 
-    @NotBlank(message = "name is required")
+    @NotBlank(message = "Job grade name is required")
     private String name;
-    @NotNull(message = "level is required")
+
+    @NotNull(message = "Level is required")
+    @Min(value = 1, message = "Level must be at least 1")
     private Integer level;
+
     private String description;
+
     private BigDecimal minSalary;
+
     private BigDecimal maxSalary;
+
+    /**
+     * Converts incoming request DTO -> JobGrade Entity
+     */
+    public JobGrade toEntity() {
+        JobGrade jobGrade = new JobGrade();
+        jobGrade.setName(this.name);
+        jobGrade.setLevel(this.level);
+        jobGrade.setDescription(this.description);
+        jobGrade.setMinSalary(this.minSalary);
+        jobGrade.setMaxSalary(this.maxSalary);
+        return jobGrade;
+    }
 }
