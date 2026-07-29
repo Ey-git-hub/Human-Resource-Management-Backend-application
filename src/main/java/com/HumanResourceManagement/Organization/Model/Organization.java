df94576c3e76f5fc7625db99163c43bf3f9501b7
eyob <eyob.tadesse@astu.edu.et>
2026-07-29 04:49:58 +0300

    chore: remove unused import in Organization model

--- src/main/java/com/HumanResourceManagement/Organization/Model/Organization.java
+++ src/main/java/com/HumanResourceManagement/Organization/Model/Organization.java
@@ -9,7 +9,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.ArrayList;
-
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
