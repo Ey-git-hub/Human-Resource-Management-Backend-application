53a22c3ca96e69f3335a770954d9e626da185c09
eyob <eyob.tadesse@astu.edu.et>
2026-07-30 10:21:52 +0300

    chore: remove unnecessary @Repository annotation from PayrollRepository

--- src/main/java/com/HumanResourceManagement/Payroll/Repository/PayrollRepository.java
+++ src/main/java/com/HumanResourceManagement/Payroll/Repository/PayrollRepository.java
@@ -3,12 +3,10 @@ package com.HumanResourceManagement.Payroll.Repository;
 import com.HumanResourceManagement.Payroll.Model.Payroll;
 import com.HumanResourceManagement.Payroll.Model.PayrollStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 
-@Repository
 public interface PayrollRepository extends JpaRepository<Payroll, Long> {
 
     List<Payroll> findByEmployeeId(Long employeeId);
