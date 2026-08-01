90c7d23c0a2ff3b2f8de8196dc53d79989786e9b
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-01 14:10:25 +0300

    feat: add pagination support for findByJobApplicationId, findByInterviewerId, and findByStatus methods in InterviewRepository

--- src/main/java/com/HumanResourceManagement/Recruitment/repository/InterviewRepository.java
+++ src/main/java/com/HumanResourceManagement/Recruitment/repository/InterviewRepository.java
@@ -2,6 +2,8 @@ package com.HumanResourceManagement.Recruitment.repository;
 
 import com.HumanResourceManagement.Recruitment.Model.Interview;
 import com.HumanResourceManagement.Recruitment.Model.Interview.Status;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 // import org.springframework.stereotype.Repository;
 
@@ -11,10 +13,13 @@ public interface InterviewRepository extends JpaRepository<Interview, Long> {
 
     // Find all interviews for a given Job Application
     List<Interview> findByJobApplicationId(Long jobApplicationId);
+    Page<Interview> findByJobApplicationId(Long jobApplicationId, Pageable pageable);
 
     // Find all interviews assigned to a specific Employee
     List<Interview> findByInterviewerId(Long interviewerId);
+    Page<Interview> findByInterviewerId(Long interviewerId, Pageable pageable);
 
     // Find interviews filtered by status (SCHEDULED, COMPLETED, CANCELLED)
     List<Interview> findByStatus(Status status);
+    Page<Interview> findByStatus(Status status, Pageable pageable);
 }
