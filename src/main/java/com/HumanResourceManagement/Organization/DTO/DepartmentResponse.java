d229682bf8185d7e338030de5e73282d8c7c0427
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-04 08:55:06 +0300

    refactor: remove unused import and fromEntity method from DepartmentResponse

--- src/main/java/com/HumanResourceManagement/Organization/DTO/DepartmentResponse.java
+++ src/main/java/com/HumanResourceManagement/Organization/DTO/DepartmentResponse.java
@@ -4,10 +4,6 @@ import lombok.Data;
 
 import java.time.LocalDateTime;
 
-import com.HumanResourceManagement.Organization.Model.Department;
-
-// import com.HumanResourceManagement.application.model.Organization.Department;
-
 @Data
 public class DepartmentResponse {
     private Long id;
@@ -17,21 +13,4 @@ public class DepartmentResponse {
     private String Description;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
-
-    public static DepartmentResponse fromEntity(Department department) {
-        DepartmentResponse response = new DepartmentResponse();
-        response.setId(department.getId());
-        response.setName(department.getName());
-        response.setDescription(department.getDescription());
-        response.setCreatedAt(department.getCreatedAt());
-        response.setUpdatedAt(department.getUpdatedAt());
-        if (department.getManager() != null) {
-            response.setManagerId(department.getManager().getId());
-            response.setManagerName(
-                    department.getManager().getFirstName() + " " + department.getManager().getLastName());
-        }
-
-        return response;
-
-    }
 }
